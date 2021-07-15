586642f6490a36bae3a485e9d352638810dacfec
xiaojunnuo <xiaojunnuo@qq.com>
2021-07-15 18:59:39 +0800

    feat: form-helper

--- src/views/crud/form/helper/index.vue
+++ src/views/crud/form/helper/index.vue
@@ -1,6 +1,14 @@
 <template>
   <fs-page>
-    <fs-crud ref="crudRef" v-bind="crudBinding" />
+    <fs-crud ref="crudRef" v-bind="crudBinding">
+      <template #actionbar-right>
+        <a-alert
+          class="ml-1"
+          type="info"
+          message="在表单字段下方可以显示该字段的帮助说明，点击右边编辑按钮查看示例效果"
+        />
+      </template>
+    </fs-crud>
   </fs-page>
 </template>
 
