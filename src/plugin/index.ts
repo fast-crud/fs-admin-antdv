406f260b576b192602870e86c9f9c8677b2442e4
xiaojunnuo <xiaojunnuo@qq.com>
2022-10-23 01:14:20 +0800

    refactor: 文本复制组件

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,11 +2,9 @@ import "./iconify";
 import "./iconfont";
 import FastCrud from "./fast-crud";
 import permission from "./permission";
-import { VueClipboard } from "@soerenmartius/vue3-clipboard";
 function install(app, options: any = {}) {
   app.use(FastCrud, options);
   app.use(permission);
-  app.use(VueClipboard);
 }
 
 export default {
