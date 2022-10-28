56cff5eb9ebf2817f2141228120541ac29d7e96d
xiaojunnuo <xiaojunnuo@qq.com>
2022-10-28 11:24:30 +0800

    fix: 修复d.ts某些地方飘红的问题

--- vite.config.ts
+++ vite.config.ts
@@ -69,6 +69,9 @@ export default ({ command, mode }) => {
       ],
       dedupe: ["vue"]
     },
+    optimizeDeps: {
+      include: ["ant-design-vue"]
+    },
     build: {
       rollupOptions: {
         plugins: [visualizer()]
