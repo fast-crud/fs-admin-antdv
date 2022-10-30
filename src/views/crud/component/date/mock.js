f822b5af2357c2b32a1511bbccab07692e307c13
xiaojunnuo <xiaojunnuo@qq.com>
2022-10-30 23:58:54 +0800

    perf: extends增加人性化时间格式化组件

--- src/views/crud/component/date/mock.js
+++ src/views/crud/component/date/mock.js
@@ -7,6 +7,7 @@ const options = {
 const list = [
   {
     timestamp: 123123123123,
+    humanize: new Date().getTime() - 11111111,
     datetime: "2019-09-01 11:11:11",
     date: "2019-09-02 11:11:11",
     format: "2019-09-21 11:11:11",
@@ -20,6 +21,7 @@ const list = [
     // timestamp: 444444555,
     datetime: "2017-09-20 11:11:11",
     date: "2019-09-20 11:11:11",
+    humanize: new Date().getTime() - 22222222,
     // time: 12313123334,
     daterangeStart: "2019-09-20 11:11:11",
     daterangeEnd: "2019-09-21 11:11:11",
