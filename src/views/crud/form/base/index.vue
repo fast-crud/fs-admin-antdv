ea531f9b410da963630214601a4e5b37ba4bcdb4
xiaojunnuo <xiaojunnuo@qq.com>
2022-12-16 15:23:45 +0800

    perf: image-format 的buildUrl支持异步

---
+++ src/views/crud/form/base/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <fs-page>
+    <fs-crud ref="crudRef" v-bind="crudBinding">
+      <template #actionbar-right>
+        <a-alert class="ml-1" type="info" message="基本表单" />
+      </template>
+    </fs-crud>
+
+    <a-modal v-drag-modal :visible="true" title="拖动测试"></a-modal>
+  </fs-page>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import { useFs } from "@fast-crud/fast-crud";
+import createCrudOptions from "./crud";
+export default defineComponent({
+  name: "FormBase",
+  setup() {
+    const { crudBinding, crudRef, crudExpose } = useFs({ createCrudOptions });
+
+    // 页面打开后获取列表数据
+    onMounted(() => {
+      crudExpose.doRefresh();
+    });
+
+    return {
+      crudBinding,
+      crudRef
+    };
+  }
+});
+</script>
