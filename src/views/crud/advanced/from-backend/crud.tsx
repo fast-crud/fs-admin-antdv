98a9e3a69f862c3a819146aa13a362dab6b861be
xiaojunnuo <xiaojunnuo@qq.com>
2023-11-23 13:37:03 +0800

    chore: editRequest 判断form.id不为空

--- src/views/crud/advanced/from-backend/crud.tsx
+++ src/views/crud/advanced/from-backend/crud.tsx
@@ -13,9 +13,9 @@ export default async function (props: CreateCrudOptionsProps): Promise<CreateCru
     return await api.GetList(query);
   };
   const editRequest = async ({ form, row }: EditReq) => {
-    if(form.id==null){
+    if (form.id == null) {
       form.id = row.id;
-    };
+    }
     return await api.UpdateObj(form);
   };
   const delRequest = async ({ row }: DelReq) => {
