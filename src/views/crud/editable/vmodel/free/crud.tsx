942870240447aa95ce8d8a1fe4b456b916bf4ba9
xiaojunnuo <xiaojunnuo@qq.com>
2023-11-23 14:09:23 +0800

    chore:

--- src/views/crud/editable/vmodel/free/crud.tsx
+++ src/views/crud/editable/vmodel/free/crud.tsx
@@ -2,22 +2,6 @@ import * as api from "./api";
 import { dict, compute, CreateCrudOptionsProps, CreateCrudOptionsRet, UserPageQuery, UserPageRes, EditReq, DelReq, AddReq } from "@fast-crud/fast-crud";
 export default function ({ crudExpose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
   const { crudBinding } = crudExpose;
-  // const pageRequest = async (query: UserPageQuery): Promise<UserPageRes> => {
-  //   return await api.GetList(query);
-  // };
-  // const editRequest = async ({ form, row }: EditReq) => {
-  //   if (form.id == null) {
-      form.id = row.id;
-    }
-  //   return await api.UpdateObj(form);
-  // };
-  // const delRequest = async ({ row }: DelReq) => {
-  //   return await api.DelObj(row.id);
-  // };
-  //
-  // const addRequest = async ({ form }: AddReq) => {
-  //   return await api.AddObj(form);
-  // };
 
   return {
     crudOptions: {
